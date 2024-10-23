e2c3f8494ff7a7b15d99c75ae044bdcbc468db42
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-10-23 06:43:43 -1100

    Update Main•menu.js

--- plugins/Main•menu.js
+++ plugins/Main•menu.js
@@ -31,10 +31,10 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
     m.react("📒");
     let menu = ``;
 
-    let txt =  ` *┏━ 𝐌𝐄𝐍Ú | 𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐀𝐈 ━┓*\n\n`
+    let txt =  ` *┏━ \`𝐌𝐄𝐍Ú | 𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐀𝐈\` ━┓*\n`
 txt+= '┃ 𝐂𝐑𝐄𝐀𝐃𝐎𝐑:' + ` https://wa.link/dn0muq\n`;
 txt+= '┃ 𝐓𝐎𝐓𝐀𝐋 𝐃𝐄 𝐔𝐒𝐔𝐀𝐑𝐈𝐎𝐒:' + ` ${rtotal}\n`;
-txt+= '┃ 𝐓𝐈𝐄𝐌𝐏𝐎 𝐀𝐂𝐓𝐈𝐕𝐎:' + ` ${uptime}\n\n┗━━━━━━━━━━━━━━━━━━┛`
+txt+= '┃ 𝐓𝐈𝐄𝐌𝐏𝐎 𝐀𝐂𝐓𝐈𝐕𝐎:' + ` ${uptime}\n┗━━━━━━━━━━━━━━━━━━┛`
 
     let listSections = [];
 
