b4ec10386326df8720eac2f2bba11610a838939f
𝐒𝐏𝐄𝐂𝐓𝐑𝐔𝐌 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 <cuentaoficialspectrum@gmail.com>
2024-10-23 07:10:05 -1100

    Update Main•allfake.js

--- plugins/Main•allfake.js
+++ plugins/Main•allfake.js
@@ -101,9 +101,9 @@ global.fake = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo
 }}, { quoted: m }
 
 global.icono = [ 
-'https://qu.ax/lwEqn.jpg',
-'https://qu.ax/jJpmr.jpg',
-'https://qu.ax/PGFnh.jpg'
+'https://qu.ax/pETLp.jpg',
+'https://qu.ax/pETLp.jpg',
+'https://qu.ax/pETLp.jpg'
 ].getRandom()
 
 global.rcanal = { contextInfo: { isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: "120363206717994793@newsletter", serverMessageId: 100, newsletterName: namechannel, }, externalAdReply: { showAdAttribution: true, title: namebot, body: dev, mediaUrl: null, description: null, previewType: "PHOTO", thumbnailUrl: icono, sourceUrl: redes, mediaType: 1, renderLargerThumbnail: false }, }, }}
